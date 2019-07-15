0990b0e87e065c96629d65cc25c52aa117712e01
Luis Veras <lveras@simetricaconsulting.com>
2019-07-15 09:51:11 -0400

    Desing changes

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -10,6 +10,7 @@ import { DataJson } from 'src/app/models/DataJson';
 export class PerfilComponent implements OnInit {
 
   public infoData: DataJson[];
+  public picRoute: string;
 
   constructor(
     private httpClient: HttpClient,
@@ -23,8 +24,10 @@ export class PerfilComponent implements OnInit {
    let jsonString = '';
    if (language === 'ES') {
       jsonString = '../../../assets/dataJSON/esData.json';
+      this.picRoute = '../../../assets/pictures/icons/ES.png';
     }  else if (language === 'EN') {
       jsonString = '../../../assets/dataJSON/enData.json';
+      this.picRoute = '../../../assets/pictures/icons/EN.png';
     }
 
    this.httpClient.get(jsonString).subscribe((data: any) => {
